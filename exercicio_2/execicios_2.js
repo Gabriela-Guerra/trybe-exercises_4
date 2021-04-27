0be32f527512daddb28cff812db7bb8327444977
Live session user <gabrielaguerra.arq@gmail.com>
2021-04-26 21:47:17 -0300

    adiciona requisito 1 do exercicio

--- exercicio_2/execicios_2.js
+++ exercicio_2/execicios_2.js
@@ -8,3 +8,12 @@ for (let name of names) {
     console.log(name);
 }
 
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index++) {
+    console.log(numbers[index]);    
+}
+
+for (let index = 0; index < numbers.length; index++) {
+    let = numbes[index];
+}
